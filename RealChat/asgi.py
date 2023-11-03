d7a2d4a9f80852b5f00e3ba5a087bf76bcfd6b87
Ubuntu <realchat@RealChat.55pu50q5lgkutdvpfbar4vailb.ax.internal.cloudapp.net>
2023-11-03 17:35:20 +0000

    Remade remote changes

--- RealChat/asgi.py
+++ RealChat/asgi.py
@@ -1,15 +1,15 @@
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-#from channels.auth import AuthMiddlewareStack
-from user.middlewares import TokenAuthMiddleWare
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RealChat.settings')
 
 django_asgi_app = get_asgi_application()
+
+from user.middlewares import TokenAuthMiddleWare
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
